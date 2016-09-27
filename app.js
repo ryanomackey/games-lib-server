3fa8af28740485f283988f29c7dd7c0f7b830507
Ryan <ryan.o.mackey@gmail.com>
2016-09-27 10:59:54 -0600

    added favicon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors');
@@ -22,8 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
